337c5dbd729dbb25eee862bbe5dfb12b3b6d7478
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2019-11-03 21:09:48 +0200

    Update verilog_parser.py

--- verilog_parser.py
+++ verilog_parser.py
@@ -5,6 +5,7 @@ class cell:
         self.library_data=f.read()
         f.close()        
         self.line_arr = line.split(" ")
+        self.io_counter = 3                               # We will have inputs or outputs starting from the fourth index of the line_arr
         self.type = self.line_arr[0].replace("\n", "")    # replace to remove any extra endl
       
         
@@ -18,11 +19,42 @@ class cell:
 
     def find_inputs(self):
         to_find = "cell (" +self.type + ")"
-        if(self.library_data.find(to_find) != -1):
-            print (self.library_data[self.library_data.index(to_find), len(to_find)])
+        starting_idex = self.library_data.find(to_find) 
+        if(starting_idex != -1):
+            current_cell_info = self.library_data.split(to_find)
+            current_cell_info = current_cell_info[1]
+            if(current_cell_info.find("cell (") != -1):
+                current_cell_info = current_cell_info.split("cell (")
+                current_cell_info = current_cell_info[0]
+                sp_current_cell_info = current_cell_info.split("direction")
+                self.inputs = list()
+                for i in range(len(sp_current_cell_info)):
+                    if(sp_current_cell_info[i].find("input") != -1):
+                         #find the input pin name in the liberty file, then go search in the verilog line
+                         port_root_name = sp_current_cell_info[i-1][sp_current_cell_info[i-1].find("pin") + 4: sp_current_cell_info[i-1].rfind(")")]
+                         port_con_name =  self.line_arr[self.io_counter][self.line_arr[self.io_counter].find(port_root_name) + 2: self.line_arr[self.io_counter].find(")")]
+                         self.io_counter += 1 
+                         self.inputs.append((port_root_name, port_con_name))
 
-
-    #def find_outputs(self, line):
+                            
+    def find_outputs(self):
+        to_find = "cell (" +self.type + ")"
+        starting_idex = self.library_data.find(to_find) 
+        if(starting_idex != -1):
+            current_cell_info = self.library_data.split(to_find)
+            current_cell_info = current_cell_info[1]
+            if(current_cell_info.find("cell (") != -1):
+                current_cell_info = current_cell_info.split("cell (")
+                current_cell_info = current_cell_info[0]
+                sp_current_cell_info = current_cell_info.split("direction")
+                self.outputs = list()
+                for i in range(len(sp_current_cell_info)):
+                    if(sp_current_cell_info[i].find("output") != -1):
+                         #find the input pin name in the liberty file, then go search in the verilog line
+                         port_root_name = sp_current_cell_info[i-1][sp_current_cell_info[i-1].find("pin") + 4: sp_current_cell_info[i-1].rfind(")")]
+                         port_con_name =  self.line_arr[self.io_counter][self.line_arr[self.io_counter].find(port_root_name) + 2: self.line_arr[self.io_counter].find(")")]
+                         self.io_counter += 1 
+                         self.outputs.append((port_root_name, port_con_name))
 
 
 
@@ -37,6 +69,9 @@ for i in result:
     if(cell_i.check_cell()):
         cells_list.append(cell_i)
     
+
 for c in cells_list:
-    print(c.name)
+    c.find_inputs()
+    c.find_outputs()
+    print(c.outputs)
 
