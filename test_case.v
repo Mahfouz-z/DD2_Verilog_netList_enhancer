fef3db73a4a7cc53945ab1c77e928e8b35b567f7
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2019-11-11 18:44:11 +0200

    Create test_case.v

---
+++ test_case.v
@@ -0,0 +1,18 @@
+module rca4 (a, b, ci, s, co);
+
+input ci;
+output co;
+input [3:0] a;
+input [3:0] b;
+output [3:0] s;
+
+
+OAI21X1 OAI21X1_2 ( .A(_4_), .B(_1_), .C(_6_), .Y(c1) );
+
+OR2X2 OR2X2_1 ( .A(c1), .B(a[1]), .Y(_12_) );
+NAND2X1 NAND2X1_1 ( .A(c1), .B(a[1]), .Y(_13_) );
+NOR2X1 NOR2X1_1 ( .A(c1), .B(a[1]), .Y(_8_) );
+AND2X2 AND2X2_1 ( .A(c1), .B(a[1]), .Y(_9_) );
+AND2X2 AND2X2_2 ( .A(c1), .B(a[1]), .Y(_9_) );
+
+endmodule
