f0eb1712e406e4701b4fd95604c08468dca0a82c
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2019-11-11 16:24:44 +0200

    Update cell_class.py

--- cell_class.py
+++ cell_class.py
@@ -1,4 +1,12 @@
+
+#This class is a simple parser class for verilog files. It needs the liberty file that will parse the verilog
+#file for in order to validate the existence of the cells in the verilog netlist. The main Functions will be
+#described in comments above each line
+
+
 class cell:
+    # The init function opens the liberty file for reading, initializes variables needed later on, and
+    # Assigns the type attribute of the cell
     def __init__(self, line):
         f=open(r"verilog parser\osu035.lib", "r")
         self.library_data=f.read()
@@ -7,7 +15,8 @@ class cell:
         self.io_counter = 3                               # We will have inputs or outputs starting from the fourth index of the line_arr
         self.type = self.line_arr[0].replace("\n", "")    # replace to remove any extra endl
 
-        
+    # The check_cell function checks if the type assigned in the intializer is a valid type that exists in
+    # the liberty file, which, if true, extracts the name from the line read from the netlist    
     def check_cell(self):
         to_find = "cell (" +self.type + ")"
         if(self.library_data.find(to_find) == -1): 
@@ -16,6 +25,8 @@ class cell:
             self.name = self.line_arr[1]
             return True   
 
+    # After checking the cells, this function is called to collect information about the inputs and outputs of
+    # each cell and save them in lists as attributes of each cell in the class
     def find_io(self):
         to_find = "cell (" +self.type + ")"
         starting_idex = self.library_data.find(to_find) 
@@ -45,19 +56,3 @@ class cell:
                          self.io_counter += 1 
                          self.outputs.append([port_root_name, port_con_name])
         
-
-                            
-  
-                    
-
-    def get_inputs(self):
-        return self.inputs
-
-    def set_out_capacitance(self, cap):
-        self.out_capacitance = cap
-
-    def set_out_num(self, number_outputs):
-        self.out_num = number_outputs
-
-    def calc_delay(self, library):
-        library.get_group
